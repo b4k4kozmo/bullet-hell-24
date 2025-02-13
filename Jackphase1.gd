1f950d9bc99c6a9e0d086294648d8f4499146111
Cosmo Steiner-Schlede <106993529+b4k4kozmo@users.noreply.github.com>
2025-02-13 10:48:14 -0800

    fixed some stuff added a proto song

--- Jackphase1.gd
+++ Jackphase1.gd
@@ -5,9 +5,7 @@ func enter():
 	owner.alpha = -.333
 	owner.bullet_type = 0
 	speed.start()
-	if Bgm.stream != load("res://sounds/Dreaming through the Jungle.mp3"):
-		Bgm.stream = load("res://sounds/Dreaming through the Jungle.mp3")
-		Bgm.play()
+
 
 func transition():
 	if can_transition:
